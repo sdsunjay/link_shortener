ea08298dc8ca8f7915c6f835520ab3785438142d
Sunjay <sdsunjay73@yahoo.com>
2019-03-15 17:51:50 -0400

    logic

--- app/models/shorten_url.rb
+++ app/models/shorten_url.rb
@@ -1,42 +1,39 @@
 class ShortenUrl < ApplicationRecord
   has_many :url_clicks
   default_scope { order(created_at: :desc) }
-  validates :original_url, presence: true
+  validates :original_url, presence: { message: "Must be given" }
   validates :original_url, url: true
 
   before_create :generate_short_url
   before_create :generate_admin_url
-  # before_save { self.errors.add(:base, 'error here or something') if self.short_url.nil? }
   # before_create :find_duplicate
 
   enum status: { active: 0, inactive: 1 }
-
-  UNIQUE_ID_LENGTH = 6
-  LETTERS = [*'a'..'z', *'A'..'Z', *0..9]
+  validates :status, inclusion: { in: statuses.keys, message: "%{value} is not a valid status" }
 
   # Check a unique URL for a given web address before saving it into the DB
   def generate_short_url
-     short_url = LETTERS.shuffle[0,UNIQUE_ID_LENGTH].join
+    short_url = unique_url
 
-     # check if this is a duplicate
-     if ShortenUrl.where(short_url: short_url).exists?
-       # try again
-       self.generate_short_url
-     else
-       self.short_url = short_url
-     end
+    # check if this is a duplicate
+    if ShortenUrl.where(short_url: short_url).exists?
+      # try again
+      generate_short_url
+    else
+      self.short_url = short_url
+    end
   end
 
   def generate_admin_url
-     admin_url = LETTERS.shuffle[0,UNIQUE_ID_LENGTH].join
+    admin_url = unique_url
 
-     # check if this is a duplicate
-     if ShortenUrl.where(admin_url: admin_url).exists?
-       # try again
-       self.generate_admin_url
-     else
-       self.admin_url = admin_url
-     end
+    # check if this is a duplicate
+    if ShortenUrl.where(admin_url: admin_url).exists?
+      # try again
+      generate_admin_url
+    else
+      self.admin_url = admin_url
+    end
   end
 
   def is_duplicate?
@@ -53,7 +50,8 @@ class ShortenUrl < ApplicationRecord
   # end
 
   def active?
-    return true if self.status == 'active'
+    return true if status == 'active'
+
     false
   end
 
@@ -65,4 +63,7 @@ class ShortenUrl < ApplicationRecord
     "http://localhost:3000/s/admin/#{admin_url}"
   end
 
+  def unique_url
+    [*'a'..'z', *'A'..'Z', *0..9].sample(6).join
+  end
 end
